8aa1ad71ef96d67648d7ce25ed644803ac95c2d3
Dmitry <irvis@imega.ru>
2015-12-03 20:36:41 +0300

    Add action get item

--- app/getItem.lua
+++ app/getItem.lua
@@ -1,36 +1,36 @@
-local json = require "cjson"
+--
+-- Copyright (C) 2015 iMega ltd Dmitry Gavriloff (email: info@imega.ru),
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-ngx.say(json.encode({ 1, 2, 'fred', {first='mars',second='venus',third='earth'} }))
-
-
+local json  = require "cjson"
 local redis = require "resty.redis"
-local red = redis:new()
 
-red:set_timeout(1000)
+local db = redis:new()
+db:set_timeout(1000)
 
-local ok, err = red:connect("cart_db", 6379)
-
+local ok, err = db:connect("cart_db", 6379)
 if not ok then
+    ngx.say(err)
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-end  
-
-ok, err = red:set("dog", "an animal")
-if not ok then
-    ngx.say("failed to set dog: ", err)
-    return
 end
 
-ngx.say("set result: ", ok)
-
-local res, err = red:get("dog")
+local res, err = db:get(ngx.var.cart_uuid .. ":" .. ngx.var.product_uuid)
 if not res then
-    ngx.say("failed to get dog: ", err)
-    return
+    ngx.say(err)
+    ngx.exit(ngx.HTTP_NOT_FOUND)
 end
 
-if res == ngx.null then
-    ngx.say("dog not found.")
-    return
-end
-
-ngx.say("dog: ", res)
+ngx.say(res)
+ngx.exit(ngx.HTTP_OK)
